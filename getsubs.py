fe28e287ea3e18332aaaa5036534b043037c69bc
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2010-02-15 01:06:32 -0800

    Now unpacks all submission files and then deletes all files, since they're all inside the submission directories anyway

--- getsubs.py
+++ getsubs.py
@@ -43,13 +43,14 @@ def getsubs2(assign):
 	proc.wait()
 	gen = os.walk(".")
 	r, d, f = gen.next()
-	regex = "cs61c-[a-z][a-z].\d{12,12}"
-	p = re.compile(regex)
-	filtered = filter(lambda x: p.match(x), f) #only unpack assignments, not autograder results
-	for file in filtered:
-		print "Looking at ", file
-		look = subprocess.Popen(["lookat", file, "-d " +file +".d"])
-		look.wait()
+	f.sort()
+	subsregex = "(cs61c-[a-z][a-z]).\d{12,12}"
+	logsregex = "[ok|problem]-" + subsregex
+	p = re.compile(subsregex)
+	for file in f:
+		if p.match(file):#only try to unpack valid submissions, not log files
+			look = subprocess.Popen(["lookat", "-d", file +".d", file])
+			look.wait()
 		os.remove(file)
 
 def makeroster(filename, d, outfile):
