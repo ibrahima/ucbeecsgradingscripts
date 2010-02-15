a58a4ef36d9c5b3344d5bd7b6a4ee8874ef851a9
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2010-02-15 01:45:36 -0800

    Don't delete the log files and make a nice tar.bz2 archive for downloading.
    Apparently the lookat script symlinks to the local copy of the log file
    when it unpacks, instead of symlinking to the actual log file. Thus, you
    can't delete the log file symlinks that are retreived by get-submissions.
    
    Also, this now archives the submissions in a format that is easily
    downloaded onto a home computer.

--- getsubs.py
+++ getsubs.py
@@ -13,13 +13,18 @@ def getsubs(assign):
 	f.sort()
 	subsregex = "(cs61c-[a-z][a-z]).\d{12,12}"
 	logsregex = "[ok|problem]-" + subsregex
-	p = re.compile(subsregex)
-	for file in f:
-		if p.match(file):#only try to unpack valid submissions, not log files
-			look = subprocess.Popen(["lookat", "-d", file +".d", file])
-			look.wait()
+	subsre = re.compile(subsregex)
+	logsre = re.compile(logsregex)
+	submissions = filter(lambda x: subsre.match(x), f)
+	logs = filter(lambda x: logsre.match(x), f)
+	for file in submissions:
+		look = subprocess.Popen(["lookat", "-d", file +".d", file])
+		look.wait()
 		os.remove(file)
-
+	os.chdir("..")
+	tar = subprocess.Popen(["gtar", "cjvf", assign+"tar.bz2", "--dereference", assign])
+	tar.wait()
+	
 if __name__ == "__main__":
 	import sys
 	getsubs(sys.argv[1])
