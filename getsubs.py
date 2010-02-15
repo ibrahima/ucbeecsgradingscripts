e314b6b65f630db83946375862e123ef4497fa9a
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2010-02-15 01:07:55 -0800

    Removed legacy stuff that isn't needed when the grading system is set up properly and readers have students assigned to them

--- getsubs.py
+++ getsubs.py
@@ -2,40 +2,7 @@ import subprocess
 import os
 import re
 
-def makedict(filename):
-	f = open(filename)
-	d={}
-	for line in f.readlines():
-		parseline(line, d)
-	return d
-
-def parseline(line, d):
-	(uid, name) = line.split()
-	d[uid]=name
-
-def getsubs(assign, rosterfile="roster"):
-	if not os.path.isfile(rosterfile):
-		makeroster("mysortedroster.txt", makedict("sorted.txt"), rosterfile)
-	d = makedict("sorted.txt")	
-	if not os.path.exists(assign):
-		os.mkdir(assign)
-	f = open("mysortedroster.txt")
-	r = open("%sroster"%assign, "w")
-
-	for uid in [x.strip() for x in f.readlines()]:
-		os.mkdir(assign + "/" + uid[-2:])
-		os.chdir(assign + "/" + uid[-2:])
-		proc = subprocess.Popen(["get-subm", assign, uid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		output, err = proc.communicate()
-		if err[:10] == "No entries":
-			r.write("{0} {1} 0 No submission\n".format(uid, d[uid]))
-		else:
-			r.write("{0} {1} 2\n".format(uid, d[uid]))
-		os.chdir("../..")
-	r.close()
-	f.close()
-
-def getsubs2(assign):
+def getsubs(assign):
 	if not os.path.exists(assign):
 		os.mkdir(assign)
 	os.chdir(assign)
@@ -53,14 +20,6 @@ def getsubs2(assign):
 			look.wait()
 		os.remove(file)
 
-def makeroster(filename, d, outfile):
-    f = open(filename)
-    r = open(outfile, "w")
-    for line in f.readlines():
-        r.write("{0} {1} 2\n".format(line.strip(), d[line.strip()].strip()))
-    r.close()
-    f.close()
-
 if __name__ == "__main__":
 	import sys
-	getsubs2(sys.argv[1])
+	getsubs(sys.argv[1])
