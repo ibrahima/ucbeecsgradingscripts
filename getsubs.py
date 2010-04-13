ab8fea0d8ac84d1b6ccb334fd3bd9004d292f92f
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2010-04-12 23:04:28 -0700

    Detects autograder results in log files and prints them out now when grabbing all results. A future version may autogenerate a roster with these scores filled in, but this would be weird because the autograder points don't usually correlate with actual class points. Also removed tar verbose output so that you can actually find the autograder result stuff in the output.

--- getsubs.py
+++ getsubs.py
@@ -14,18 +14,37 @@ def getsubs(assign):
 	r, d, f = gen.next()
 	f.sort()
 	subsregex = "(cs61c-[a-z][a-z]).\d{12,12}"
-	logsregex = "[ok|problem]-" + subsregex
+	logsregex = "ok|problem-" + subsregex
 	subsre = re.compile(subsregex)
 	logsre = re.compile(logsregex)
 	submissions = filter(lambda x: subsre.match(x), f)
 	logs = filter(lambda x: logsre.match(x), f)
+
 	for file in submissions:
 		look = subprocess.Popen(["lookat", "-d", file +".d", file])
 		look.wait()
 		os.remove(file)
+	for log in logs:
+		getAutograderResult(log)
 	os.chdir("..")
-	tar = subprocess.Popen(["gtar", "cjvf", assign+".tar.bz2", "--dereference", assign])
+	print "Creating ", assign, ".tar.bz2"
+	tar = subprocess.Popen(["gtar", "cjf", assign+".tar.bz2", "--dereference", assign])
 	tar.wait()
+	
+	
+def getAutograderResult(logfile):
+	scoreregex = "^Score: (\d{1,2})/(\d{1,2})$"
+	cmp = re.compile(scoreregex)
+	file = open(logfile)
+	score = 0
+	for line in file:
+		if line.startswith("Score: "):
+			m = cmp.match(line)
+			if m:
+				score = m.groups()[0]
+	print "Detected a score of ", score, "in ", logfile
+	return score 
+
 	
 if __name__ == "__main__":
 	import sys
